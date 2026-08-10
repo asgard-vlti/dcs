2103df35ebbad981d8e7666d2e2631b528ac1a23
Marc White <mwhite1206@gmail.com>
2026-08-10 13:42:23 -0400

    Revert previous changes

--- commander/include/commander/commander.h
+++ commander/include/commander/commander.h
@@ -18,18 +18,8 @@ bool acquire_single_instance_lock(const char *lock_path);
 void unacquire_single_instance_lock();
 int set_log_level(int level);
 
-// Canonical logging shortcuts. Use these in new code.
-#define COMMANDER_ERROR(...) logprintf(LOG_ERROR, __VA_ARGS__)
-#define COMMANDER_WARN(...) logprintf(LOG_WARNING, __VA_ARGS__)
-#define COMMANDER_INFO(...) logprintf(LOG_INFO, __VA_ARGS__)
-#define COMMANDER_DEBUG(...) logprintf(LOG_DEBUG, __VA_ARGS__)
-
-// Backward-compatible aliases for legacy users.
-// Define COMMANDER_DISABLE_LEGACY_LOG_MACROS before including this header to
-// avoid global macro names like `info` that may collide with third-party headers.
-#ifndef COMMANDER_DISABLE_LEGACY_LOG_MACROS
-#define error(...) COMMANDER_ERROR(__VA_ARGS__)
-#define warn(...) COMMANDER_WARN(__VA_ARGS__)
-#define info(...) COMMANDER_INFO(__VA_ARGS__)
-#define debug(...) COMMANDER_DEBUG(__VA_ARGS__)
-#endif
+// Shortcuts for common log levels (using heimdallr's settings.s.loglevel)
+#define error(...) logprintf(LOG_ERROR, __VA_ARGS__)
+#define warn(...) logprintf(LOG_WARNING, __VA_ARGS__)
+#define info(...) logprintf(LOG_INFO, __VA_ARGS__)
+#define debug(...) logprintf(LOG_DEBUG, __VA_ARGS__)
